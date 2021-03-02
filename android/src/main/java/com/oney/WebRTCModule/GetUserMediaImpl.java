c343279b0923caa62e448b35a4ccb00bca91314a
Ivo Kacharov <ivor.kach@gmail.com>
2021-03-02 13:38:38 +0200

    reduce screen share size by half

--- android/src/main/java/com/oney/WebRTCModule/GetUserMediaImpl.java
+++ android/src/main/java/com/oney/WebRTCModule/GetUserMediaImpl.java
@@ -322,8 +322,8 @@ class GetUserMediaImpl {
 
     private VideoTrack createScreenTrack() {
         DisplayMetrics displayMetrics = getDisplayMetrics();
-        int width = displayMetrics.widthPixels;
-        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels * 0.5;
+        int height = displayMetrics.heightPixels * 0.5;
         int fps = 30;
         ScreenCaptureController screenCaptureController = new ScreenCaptureController(width, height, fps, mediaProjectionPermissionResultData);
         return createVideoTrack(screenCaptureController);
